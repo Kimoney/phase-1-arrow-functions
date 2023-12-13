ae817ef939f636d8837bfe39a79b198eaf187783
John <johntaykimani@gmail.com>
2023-12-13 12:34:47 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// a function expressiion for dividing
+
+const divide = function(nume=2000,deno=100){
+    return nume/deno
+}
+
+// Arrow Function called square to mutliply number by itself
+
+const square = numberFlani => numberFlani**2
+
+//  Arrow function that takes two parameters and adds them together
+
+const add = (nambaOne, nambaTwo) => nambaOne + nambaTwo
